82b70dfc69f70f3aba677d89aa27012d1d7f1d33
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-02-22 12:15:45 -0500

    have basic structure to modular add ons and components

---
+++ bin/bases/default/vue/home/component.vue
@@ -0,0 +1,31 @@
+
+<template lang="pug">
+
+div.bordered
+	h2 {{welcome}}
+	p {{message}}
+
+</template>
+
+<script>
+
+module.exports = {
+	data() {
+		return {
+			welcome: `Hello!`,
+			message: `Welcome to our home component.`
+		}
+	}
+}
+
+</script>
+
+<style lang="sass">
+
+.bordered {
+	border: dashed 1px gray;
+	padding: 20px;
+	margin: 10px;
+}
+
+</style>
