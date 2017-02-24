6b5d89c59d970835c54b27967beca367084371b8
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-02-23 19:54:08 -0500

    working on loading auth'd user information

--- bin/extras/user/component.vue
+++ bin/extras/user/component.vue
@@ -17,6 +17,33 @@ module.exports = {
 				username: 'username'
 			}
 		}
+	},
+	created() {
+		this.getUser();
+	},
+	watch: {
+		'$route': 'getUser'
+	},
+	methods: {
+		getUser() {
+			var headers = {};
+			headers[lib.config.USER_AUTH_API_TOKEN_HEADER] = lib.cookies.get(lib.config.USER_AUTH_API_TOKEN);
+
+			lib.$.ajax({
+				url: lib.config.API_ENDPOINT + '/user',
+				type: 'get',
+				headers: headers,
+				success: res => {
+					console.log(res);
+					this.user = res.data;
+				},
+				error: xhr => {
+
+				}
+			});
+
+		}
+
 	}
 }
 
